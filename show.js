133d58e6429889dc7ea735ab46d899de02a899cd
anishsarangi <anish.kumar.sarangi@gmail.com>
2017-03-20 03:05:05 +0530

    upadate existing file

--- show.js
+++ show.js
@@ -1,5 +1,44 @@
 $(document).ready(function()
 {
-	var title=$('title')[0].text;
-	console.log(title);
+	var url=window.location.href;
+	$("a").mouseenter(function()
+	{
+			var str=$(this).attr('href');
+		if(str.match("^//"))
+		{
+			var current_url="https:"+str;
+		}
+		else if(str.match("^/"))
+		{
+			var current_url=url-"/"+str;
+		}
+		else
+		{
+			var current_url=str;
+		}
+		$(this).append("<div class='popup'></div>");
+		$(".popup").append(current_url);
+		var pos = $(this).position();
+		var top = pos.top-"20";
+		var left = pos.left-"30";
+		$(".popup").css({
+		 position:"absolute",
+		 top:top,
+		 left:left,
+		"z-index":"1",
+		"display":"block",
+		"width":"auto",
+		"height":"auto",
+		"padding":"5",
+		"color":"#FFF",
+		"background":"#535663",
+		"font-size":"11",
+		"text-decoration":"none",
+		"border-radius":"6"
+		});
+    })
+    .mouseleave(function()
+    {
+    	$(".popup").remove();
+    });
 });
